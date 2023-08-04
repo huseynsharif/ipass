9463eaedcec975446119c7f97c49b41ea6c42062
Huseyn Sharifzade <121455530+huseynsharif@users.noreply.github.com>
2023-08-04 14:53:07 +0400

    First Commit

---
+++ IPass/PasswordGenerator.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+
+namespace IPass { 
+
+    class PasswordGenerator
+    {
+        private Random random = new Random();
+        private bool isMix = false;
+        public string GenerateNumericPassword(int length)
+        {
+            string simvollar = "0123456789";
+            return GeneratePassword(length, simvollar);
+        }
+
+        public string GenerateAlphaPassword(int length)
+        {
+            string simvollar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+            return GeneratePassword(length, simvollar);
+        }
+
+        public string GenerateAlphaNumericPassword(int length)
+        {
+            isMix = true;
+            string simvollar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            return GeneratePassword(length, simvollar);
+        }
+
+        public bool ContainsNumber(string text)
+        {
+            foreach (char character in text)
+            {
+                if (char.IsDigit(character))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        public bool ContainsLetter(string text)
+        {
+            foreach (char character in text)
+            {
+                if (char.IsLetter(character))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+
+        public string GeneratePassword(int length, string simvollar)
+        {
+            Start:
+            string password = "";
+            for (int i = 0; i < length; i++)
+            {
+                int index = random.Next(simvollar.Length);
+                password += simvollar[index];
+            }
+
+            if(isMix) { 
+                if(ContainsLetter(password) && ContainsNumber(password))
+                {
+                    return password;
+                }
+                else
+                {
+                    goto Start;
+                }
+            }
+            else
+            {
+                return password;
+            }
+            
+        }
+    }
+}
---
+++ IPass/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PasswordGenerator passwordGenerator = new PasswordGenerator();
+            while (true) { 
+                Console.Write("Parol uzunluğunu daxil edin: ");
+                int length = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("1) Yalniz reqemlerden ibaret");
+                Console.WriteLine("2) Yalniz heriflerden ibaret");
+                Console.WriteLine("3) Yuxaridakilarin her ikisi");
+                Console.WriteLine("0) Cixis");
+                Console.Write("Seciminiz-> ");
+                string secim = Console.ReadLine();
+                Console.Write("Sizin parolunuz: ");
+                switch (secim) {
+                    case "1": Console.WriteLine(passwordGenerator.GenerateNumericPassword(length)); break;
+                    case "2": Console.WriteLine(passwordGenerator.GenerateAlphaPassword(length)); break;
+                    case "3": Console.WriteLine(passwordGenerator.GenerateAlphaNumericPassword(length)); break;
+                    case "0": Environment.Exit(0); break;
+                    default: Console.WriteLine("Secim dogru deyil"); break;
+                }
+            
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+    }
+}
